032fc30f1adce0ac3e762027ace997e17024d704
Hōtarō Oreki <TheZjarek@gmail.com>
2022-01-26 07:23:05 +0100

    first commit

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
+
+class CommentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $comments = Comment::orderBy('created_at', 'asc')->get();
+        return view("comments", ['comments' => $comments]);
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $comment = new Comment;
+        return view('commentsForm', ['comment' => $comment]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    { // Podstawowa walidacja formularza:
+        $this->validate($request, [
+            'message' => 'required|min:10|max:255',
+        ]);
+
+        if (Auth::user() == null) {
+            return view('home'); // niezalogowany, na stronę domową, już
+        }
+        $comment = new Comment();
+        $comment->user_id = Auth::user()->id;
+        $comment->message = $request->message;
+        if ($comment->save()) {
+            return redirect('comments');
+        }
+        return view('comments');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
